c1a354b46585571610f7c66180c08a546da27b65
Ban Vuong <vuongdieuban@gmail.com>
2018-12-22 19:29:22 -0700

    CRUD  customers

--- routes/customers.js
+++ routes/customers.js
@@ -12,14 +12,13 @@ const Customer = mongoose.model('Customer', new mongoose.Schema({
         maxlength: 50
     },
     phone: {
-        type: Number,
+        type: String,
         required: true,
         minlength: 5,
         maxlength: 10
     },
     isGold: {
-        type: boolean,
-        required: true,
+        type: Boolean,
         default: false,
     }
 }));
@@ -33,19 +32,21 @@ const Customer = mongoose.model('Customer', new mongoose.Schema({
 
 router.get('/', async (req, res) => {
     const customers = await Customer.find().sort("name");
-    res.send(customers)
+    res.send(customers);
 })
 
 router.post('/', async (req, res) => {
-    const result = validateReq(res.body);
-    console.log(result);
-    if (result.error) return res.status(400).send(result.error.details[0].message)
+    const result = validateReq(req.body);
+    // console.log(result.error);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
     let new_customer = new Customer({
         name: req.body.name,
         phone: req.body.phone,
         isGold: req.body.isGold,
-    })
-    new_customer = new_customer.save();
+    });
+
+    new_customer = await new_customer.save();
     res.send(new_customer);
 })
 
@@ -56,29 +57,34 @@ router.post('/', async (req, res) => {
  * PUT request for updating <id> customer
  * DELETE for deleting <id> customer
  *-----------------------------------------*/
+
 router.get('/:id', async (req, res) => {
     const customer = await Customer.findById(req.params.id);
-    if (!customer) return res.status(404).send('There is no genre with this specifid id');
+    if (!customer) return res.status(404).send('There is no customer with this specifid id');
     res.status(200).send(customer);
 })
 
+
 router.put('/:id', async (req, res) => {
     const result = validateReq(req.body);
+    console.log(result.error);
     if (result.error) return res.status(400).send(result.error.details[0].message);
-    const customer = await Customer.findByIdAndUpdate({ _id: req.params.id },
-        {
-            name: req.body.name,
-            phone: req.body.phone,
-            isGold: req.body.isGold,
-        },
-        { new: true });
+
+    // Update
+    const customer = await Customer.findByIdAndUpdate({ _id: req.params.id }, {
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold,
+    }, { new: true });
+
     if (!customer) return res.status(404).send('The customer with the given ID was not found.');
     res.send(customer);
 })
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
     const customer = await Customer.findByIdAndRemove(req.params.id);
-    if (!customer) return res.status(404).send('There is no genre with this specifid id');
+    if (!customer) return res.status(404).send('There is no customer with this specifid id');
     res.send(customer);
 })
 
@@ -86,7 +92,7 @@ router.delete('/:id', (req, res) => {
 function validateReq(customer) {
     const schema = {
         name: Joi.string().min(5).max(50).required(),
-        phone: Joi.number().min(5).max(10).required(),
+        phone: Joi.string().min(5).max(10).required(),
         isGold: Joi.boolean()
     };
     const result = Joi.validate(customer, schema);
