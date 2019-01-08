ae660aa3eb33d9a5de7f8bd220355a58b1253988
Ban Vuong <vuongdieuban@gmail.com>
2019-01-08 00:21:20 -0700

    fix winston loggin stack trace

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,8 @@
-const winston = require('winston');
+const { logger } = require("../startup/logging");
+const winston = require("winston");
+const { format, transports } = winston;
 
-module.exports = function (err, req, res, next) {
-    winston.error(err.message, err);
-
-    res.status(500).send('Something failed on the server.');
-}
+module.exports = function(err, req, res, next) {
+  logger.error({ message: err.stack });
+  res.status(500).send("Something failed on the server.");
+};
--- startup/logging.js
+++ startup/logging.js
@@ -1,33 +1,26 @@
-const winston = require('winston');
+const winston = require("winston");
 const { format, transports } = winston;
-const { combine, colorize, prettyPrint, simple } = format;
-require('express-async-errors'); // module for handling route errors, similar functionality to '../middleware/async'
+require("express-async-errors"); // module for handling route errors, similar functionality to '../middleware/async'
 
 const logger = winston.createLogger({
-    format: combine(
-        prettyPrint(),
-        colorize()
-    ),
-    transports: [
-        new transports.File({ filename: `${__dirname}/../logs/error.log`, level: "error" }),
-        new transports.File({ filename: `${__dirname}/../logs/combined.log` }),
-        new transports.Console({ level: 'debug' })
-    ]
+  format: format.simple(),
+  transports: [
+    new transports.File({ filename: `${__dirname}/../logs/combined.log` }),
+    new transports.Console()
+  ],
+  exceptionHandlers: [
+    new transports.File({
+      filename: `${__dirname}/../logs/uncaughtException.log`
+    }),
+    new transports.Console()
+  ]
 });
 
 function exceptionHandler() {
-    // Call exceptions.handle with a transport to handle exceptions
-    logger.exceptions.handle(
-        new transports.File({ filename: `${__dirname}/../logs/uncaughtException.log` }),
-        new transports.Console({
-            format: format.simple()
-        })
-    );
-
-    // Unhandle Promise Rejection   
-    process.on('unhandledRejection', (ex) => {
-        throw ex; // handled by logger.exceptions.handle()
-    })
+  // Unhandle Promise Rejection
+  process.on("unhandledRejection", ex => {
+    throw ex; // handled by logger
+  });
 }
 
 module.exports.logger = logger;
