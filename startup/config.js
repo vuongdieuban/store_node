0e29636956ff6f625b5a4e6c236e0e55c4cdf561
Ban Vuong <vuongdieuban@gmail.com>
2019-01-08 22:10:20 -0700

    logging error on startup/config.js file

--- startup/config.js
+++ startup/config.js
@@ -1,11 +1,16 @@
-const config = require('config');
+const config = require("config");
+const { logger } = require("../startup/logging");
 
-module.exports = function () {
-    /**set jwtPrivateKey in terminal
-     * in PowerShell: $env:video_store_jwtPrivateKey="optionalPrivateKeyString" 
-     * in GitBash: export video_store_jwtPrivateKey="optionalPrivateKeyString" */
-    if (!config.get('jwtPrivateKey')) {
-        throw new Error('FATAL ERROR: jwtPrivateKey is not defined');
+module.exports = function() {
+  /**set jwtPrivateKey in terminal
+   * in PowerShell: $env:video_store_jwtPrivateKey="optionalPrivateKeyString"
+   * in GitBash: export video_store_jwtPrivateKey="optionalPrivateKeyString" */
+
+  try {
+    if (!config.get("jwtPrivateKey")) {
+      throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
     }
-}
-
+  } catch (err) {
+    logger.error({ message: err.stack });
+  }
+};
